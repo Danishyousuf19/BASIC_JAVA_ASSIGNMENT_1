474ebf4b7cb535a613af5b7ca2df0009bea85860
Danishyousuf19 <130566129+Danishyousuf19@users.noreply.github.com>
2023-04-15 16:22:30 +0530

    Update Q6.java

--- Q6.java
+++ Q6.java
@@ -1,4 +1,9 @@
-
+/*Write a method that finds the number of occurrences of a specified character in a string using 
+the following header:
+public static int count(String str, char a)
+For example, count ("Welcome", 'e') returns 2. 
+Write a java program that prompts the user to enter a string followed by a character and 
+displays the number of occurrences of the character in the string*/
 public class Q6 {
 	public static int count(String str, char a) {
 		int number=0;
